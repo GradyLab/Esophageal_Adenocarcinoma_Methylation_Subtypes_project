6727d3deac0d3cd40572048a7c1aff46b2929481
smaden <metamaden@users.noreply.github.com>
2017-03-22 16:32:43 -0700

    Update InfiniumPurifyR.R

--- R/tumor_purity/InfiniumPurifyR.R
+++ R/tumor_purity/InfiniumPurifyR.R
@@ -1,8 +1,26 @@
+# Function Description: Estimate and depict tumor purity/enrichment using Zhang et al 2015's InfiniumPurify method.
+# Author: SKM
+# Original code ported to R from: https://bitbucket.org/zhengxiaoqi/infiniumpurify
+# Variables:
+#   df = methylation data (rows = markers, cols = samples)
+#   calcPurity = whether to calculate purity
+#   returnDMPonly = whether to only return differentially methylated probes (T vs. N)
+#   plotunimod = whether to return plot of overlaid sample unimodal distributions for CpGs used in purity estimates
+#   hyper = hypervariable (T vs. N) DMP list for purity estimations
+#   hypo = hypomethylated (T vs. N) DMP list for purity estimations
+#   getDMPs = whether to return list of DMPs (T vs. N, needs dmpVariablename and dmpVariancefiltTissue)
+#   dmpVariablename = name of variable in pData("name-of-minfi-object")
+#   dmpVariancefiltTissue = name of tissue in dmpVariablename variable that is hyperavariable/Tumor
+#   nsigprobes= number of significant probes to return from DMP calculations
+#   varfilt= filter for variance in DMP calculations
+#   plotunimod.main = main title name for unimodal plot (needs plotunimod = TRUE) 
+#   plotunimod.ymax= max for y-axis on unimodal plot (needs plotunimod = TRUE)
+
 InfiniumPurifyR <- function(df,calcPurity=TRUE,
                             returnDMPonly=FALSE,plotunimod=FALSE,
                             hyper=NULL,hypo=NULL,
-                            getDMPs=FALSE,dmpVariablename=NULL, # rename to dmpTissuecol
-                            dmpVariancefiltTissue=NULL, # rename to dmpVarianceTissue
+                            getDMPs=FALSE,dmpVariablename=NULL,
+                            dmpVariancefiltTissue=NULL,
                             nsigprobes=NULL,varfilt=NULL,
                             plotunimod.main=NULL,plotunimod.ymax=NULL){
   
