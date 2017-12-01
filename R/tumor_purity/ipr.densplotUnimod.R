0a53e745e988a552dd4422e1cd35907fa2f998e2
smaden <metamaden@users.noreply.github.com>
2017-12-01 14:43:39 -0800

    Delete ipr.densplotUnimod.R

--- R/tumor_purity/ipr.densplotUnimod.R
+++
@@ -1,8 +0,0 @@
-ipr.densplotUnimod <- function(df,hyper,hypo,main="Unimodal Methylation by Sample",ymax=10){
-  plot(2,1,type="b",xlim=c(0,1),ylim=c(0,ymax),
-       xlab="Beta-val",ylab="Density",main=main)
-  plotcol <- colours()[sample(nrow(df),nrow(df))]
-  for(samplei in 1:nrow(df)){
-    lines(density(ipr.dmpModmethy(betas=df[samplei,],hyper=hyper,hypo=hypo),na.rm=TRUE),col=plotcol[samplei])
-  }
-}
